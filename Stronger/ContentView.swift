4bc3e2401450fd0af10ef8798cc80ad2c9cb5dcc
kingjinho <jinhoshin319@gmail.com>
2024-01-04 21:37:35 +0900

    Initial Commit

---
+++ Stronger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stronger
+//
+//  Created by Jinho Shin on 1/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stronger/StrongerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrongerApp.swift
+//  Stronger
+//
+//  Created by Jinho Shin on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct StrongerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
